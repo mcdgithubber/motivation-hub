6f0d26c64ad2265d2bc5df41053bd3e5e04b9e4a
Ahmad Alzafiri <ahmadalzafiri1@gmail.com>
2025-04-25 23:07:49 +1200

    Update quotes-api.js

--- js/quotes-api.js
+++ js/quotes-api.js
@@ -1,21 +1,29 @@
+const API_KEY = '65DFEShVKfZW4VRoSfh9jA==2MT5wVE3A3EOFNHf';
+
 async function fetchRandomQuotes(count = 3) {
   const quotes = [];
 
   for (let i = 0; i < count; i++) {
     try {
-      const res = await fetch('https://zenquotes.io/api/random');
+      const res = await fetch('https://api.api-ninjas.com/v1/quotes', {
+        method: 'GET',
+        headers: {
+          'X-Api-Key': API_KEY
+        }
+      });
+
       if (!res.ok) throw new Error('API error');
       const data = await res.json();
 
       quotes.push({
-        content: data[0].q,
-        author: data[0].a
+        content: data[0].quote,
+        author: data[0].author
       });
 
     } catch (err) {
       console.error('⚠️ Failed to fetch quote:', err);
       quotes.push({
-        content: 'Stay strong. Your story isn’t over yet.',
+        content: 'The only way out is through.',
         author: 'Unknown'
       });
     }
